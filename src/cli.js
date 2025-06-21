16fa44ba97cf536272f6d2f84532f400d3255cf0
Daniel <dannycaro99@hotmail.com>
2025-06-20 20:02:41 -0500

    feat: Añadir la lógica del juego principal

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+export function sayHiToUser (){
+console.log("¡Bienvenido a Brain Games!")
+
+const name = readlineSync.question("¿Cuál es tu nombre?");
+
+console.log(`¡Hola, ${name}!`);
+}
