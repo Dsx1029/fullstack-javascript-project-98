8eac5446085ae571070d5804906e50571dea4a41
Daniel <dannycaro99@hotmail.com>
2025-07-01 12:39:54 -0500

    fix: Ajuste final en los scripts bin para usar el motor centralizado

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import greetUser from '../src/cli.js'; // Importar la función de bienvenida
 import runGame from '../src/index.js'; // Importar el motor del juego
 import evenGame from '../src/games/even.js'; // Importar la lógica específica del juego
 
-const userName = greetUser(); // Obtener el nombre del usuario primero
 runGame(evenGame, userName); // Luego ejecutar el juego con el nombre obtenido
